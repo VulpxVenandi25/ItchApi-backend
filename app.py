1f43194e8e747d319d38c726e0949eadf7b7ba14
VulpxVenandi25 <168295706+VulpxVenandi25@users.noreply.github.com>
2025-07-22 18:26:27 -0500

    Archivos Primarios

---
+++ app.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+CORS(app)  # Habilita CORS para todas las rutas
+
+# Configuración del puerto
+PORT = int(os.getenv("PORT", 1003))
+
+@app.route("/apigame/<int:game_id>", methods=["GET"])
+def get_game(game_id):
+    try:
+        api_key = int(os.getenv("API_KEY"))
+        headers = {"Authorization": api_key}
+        
+        # Hacemos la petición a la API de itch.io
+        response = requests.get(
+            f"https://api.itch.io/games/{game_id}",
+            headers=headers
+        )
+        response.raise_for_status()  # Lanza excepción si hay error HTTP
+        
+        return jsonify(response.json())
+    
+    except requests.exceptions.RequestException as e:
+        print(f"Error al obtener los datos del juego: {e}")
+        return jsonify({"error": "Error al obtener los datos del juego"}), 500
+
+if __name__ == "__main__":
+    print(f"Servidor backend en ejecución en http://localhost:{PORT}")
+    app.run(port=PORT, debug=True)
